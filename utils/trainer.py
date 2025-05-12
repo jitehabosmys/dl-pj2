7925488f33fbe4a9f113e3bed7479d54026103e7
jitehabosmys <2524532849@qq.com>
2025-05-12 16:57:59 +0800

    启用cudnn.benchmark以提高训练性能

--- utils/trainer.py
+++ utils/trainer.py
@@ -8,8 +8,8 @@ def set_seed(seed=42):
     torch.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)
     np.random.seed(seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    # torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
 
 def train(model, train_loader, criterion, optimizer, device, epochs=10, scheduler=None, validation_loader=None, patience=5):
     """训练模型
