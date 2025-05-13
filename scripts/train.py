913e3b7fb3135ff2a66f7d015dd0cc66d3a26b12
jitehabosmys <2524532849@qq.com>
2025-05-13 12:06:00 +0800

    归一化数值改回原来的设置

--- scripts/train.py
+++ scripts/train.py
@@ -236,12 +236,12 @@ def main():
         transforms.RandomCrop(32, padding=4),  # 添加数据增强
         transforms.RandomHorizontalFlip(),      # 添加数据增强
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     ])
     
     transform_test = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.247, 0.243, 0.261))
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
     ])
     
     # 加载训练集
